7e5d23856d881f9281f3509b123723ca3e64bd23
thadhodges <77890659+thadhodges@users.noreply.github.com>
2023-11-02 18:28:08 -0500

    guess the number game

--- FirstLesson.py
+++ FirstLesson.py
@@ -1,3 +1,28 @@
 print("Hello")
 num = 2+2
 print(num)
+
+import random
+
+def guess_the_number():
+    number = random.randint(1, 100)
+    attempts = 0
+
+    while True:
+        try:
+            guess = int(input("Guess a number between 1 and 100: "))
+            attempts += 1
+
+            if guess < number:
+                print("Too low!")
+            elif guess > number:
+                print("Too high!")
+            else:
+                print(f"Congratulations! You guessed it in {attempts} attempts.")
+                break
+        except ValueError:
+            print("Please enter a valid number.")
+
+if __name__ == "__main__":
+    guess_the_number()
+
